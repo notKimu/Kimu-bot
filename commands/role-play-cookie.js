60df2db873003724d29df5a53c09ddf15d75561a
KomradeFMX <fm235mh@gmail.com>
2023-04-01 17:26:01 +0200

    Finished the role play command base

--- commands/role-play-cookie.js
+++ commands/role-play-cookie.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 //Base de datos del Roleo
-const arrayRoleo = require("../src/json/roleo.json");
+const rolePlayArray = require("../src/json/roleplay.json");
+
+var timeoutUsers = [];
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,6 +13,9 @@ module.exports = {
 
 
     async execute(interaction) {
+
+        // Check if user is on cooldown
+        if (timeoutUsers.includes(interaction.user.id)) return interaction.reply({ content: 'You have to wait before using this command again! <:nose:1085261670043103232>', ephemeral: true });
 
         //Función para escoger un número entre dos valores (min y max)
         function randomValue(min, max) {
@@ -24,20 +29,26 @@ module.exports = {
         try {
             var member = await interaction.guild.members.fetch(user.id);
         } catch (error) {
-			return await interaction.reply({ content: "I don´t think that user is from this server! <:michiru_toast:1087450095047409734>", ephemeral: true });
+            return await interaction.reply({ content: "I don´t think that user is from this server! <:michiru_toast:1087450095047409734>", ephemeral: true });
         }
 
         if (member == interaction.member) return interaction.reply({ content: 'No te puedes dar una galletita a tí mism@', ephemeral: true });
 
         //Obtener la lista del array en el JSON
-        let gifsGalletitas = arrayRoleo[0].fotos.galletitas;
+        let cookieGifs = rolePlayArray[0].images.cookies;
 
         //El embed con el gif correspondiente
-        const galletitasEmbed = new EmbedBuilder()
+        const cookieEmbed = new EmbedBuilder()
             .setColor(interaction.guild.members.me.displayHexColor)
-            .setImage(gifsGalletitas[randomValue(0, gifsGalletitas.length)])
-            .setFooter({ text: `${interaction.guild.name} - Roleo`, iconURL: `${interaction.guild.iconURL()}` });
+            .setImage(cookieGifs[randomValue(0, cookieGifs.length)])
+            .setFooter({ text: `${interaction.guild.name} - Role Play`, iconURL: `${interaction.guild.iconURL()}` });
 
-        await interaction.reply({ content: `**${interaction.member.displayName}** ha dado una galletita a ${member} owo`, embeds: [galletitasEmbed] });
+        await interaction.reply({ content: `**${interaction.member.displayName}** gave a cookie to ${member} owo`, embeds: [cookieEmbed] });
+
+        // Add a timeout to the command
+        timeoutUsers.push(interaction.user.id);
+        setTimeout(() => {
+            timeoutUsers.shift();
+        }, 30000)
     },
 };
--- commands/role-play-hug.js
+++ commands/role-play-hug.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 // The role play array
-const rolePlayArray = require("../src/json/roleo.json");
+const rolePlayArray = require("../src/json/roleplay.json");
+
+var timeoutUsers = [];
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,6 +13,9 @@ module.exports = {
 
 
     async execute(interaction) {
+
+        // Check if user is on cooldown
+        if (timeoutUsers.includes(interaction.user.id)) return interaction.reply({ content: 'You have to wait before using this command again! <:nose:1085261670043103232>', ephemeral: true });
 
         //Función para escoger un número entre dos valores (min y max)
         function randomValue(min, max) {
@@ -24,10 +29,11 @@ module.exports = {
         try {
             var member = await interaction.guild.members.fetch(user.id);
         } catch (error) {
-			return await interaction.reply({ content: "I don´t think that user is from this server! <:michiru_toast:1087450095047409734>", ephemeral: true });
+            return await interaction.reply({ content: "I don´t think that user is from this server! <:michiru_toast:1087450095047409734>", ephemeral: true });
         };
 
         if (member === interaction.member) return interaction.reply({ content: 'It´s very sad to hug yourself :c', ephemeral: true });
+        if (member.bot) return interaction.reply({ content: 'You can´t hug a bot!', ephemeral: true });
 
         //Obtener la lista del array en el JSON
         let hugGifs = rolePlayArray[0].images.hugs;
@@ -39,5 +45,12 @@ module.exports = {
             .setFooter({ text: `${interaction.guild.name} - Role Play`, iconURL: `${interaction.guild.iconURL()}` });
 
         await interaction.reply({ content: `**${interaction.member.displayName}** gave a hug to ${member} iwi`, embeds: [abrazoEmbed] });
+
+
+        // Add a timeout to the command
+        timeoutUsers.push(interaction.user.id);
+        setTimeout(() => {
+            timeoutUsers.shift();
+        }, 30000)
     },
 };
