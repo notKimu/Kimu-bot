644b6e75bada4b81361682fbeee11d5f37029765
KomradeFMX <fm235mh@gmail.com>
2023-04-03 12:48:17 +0200

    Added a log file for the errors, gotta love JS

--- events/interaction-create.js
+++ events/interaction-create.js
@@ -1,4 +1,5 @@
 const { Events } = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -17,6 +18,7 @@ module.exports = {
 		} catch (error) {
 			console.error(`[WARNING] I had an error executing this command: ${interaction.commandName}`);
 			console.error(error);
+			fs.appendFile('../src/err/error.log', `/ An error ocurred at ${interaction.commandName} => ${error} /`);
 			return interaction.reply({ content: "Sorry! There was an internal error, please try again '^^", ephemeral: true });
 		}
 	},
